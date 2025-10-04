b51db67b584d4e9a9c41dd49b98730948f1e25e2
bluewater <70036495+Victor-wind@users.noreply.github.com>
2025-10-04 00:09:37 -0500

    Update moobuzz.py

--- solution/2019/dec/moobuzz.py
+++ solution/2019/dec/moobuzz.py
@@ -1,44 +1,33 @@
-with open('lineup.in') as f:
+with open('moobuzz.in') as f:
     lines = f.readlines()
 
 N = int(lines[0])
-cows = ['Sue', 'Bessie', 'Buttercup', 'Belinda', 'Beatrice',
-        'Bella', 'Blue', 'Betsy']
+result = -1
 
-cows.sort()
-nums = [i for i in range(1,9)]
-cows_dic = dict(zip(cows,nums))
 
-graph = [[] for _ in range(9)]
-# print(cows_dic)
-for i in range(N):
-    words = lines[i+1].split()
-    node_i = cows_dic[words[0]]
-    node_j = cows_dic[words[-1]]
-    graph[node_i].append(node_j)
-    graph[node_j].append(node_i)
+def get_numbers(k):
+    # return the numerical numbers within position [1,K] excluding fizz, buzz, fizzbuzz
+    if k <= 0: return 0
+    fizz_cnt = k//3
+    buzz_cnt = k//5
+    fizzbuzz_cnt = k//15
+    return k - fizz_cnt - buzz_cnt + fizzbuzz_cnt
 
-edge_nodes = [i for i in range(1, 9) if len(graph[i]) < 2]
-# print(f'{graph=}')
-# print(f'{edge_nodes=}')
-result_num = list()
-visited = [False] * 9
-for i in edge_nodes:
-    if visited[i]: continue
-    # it should use queue, but in this problem, there is at most 1 element in queue, we can use list as stack
-    st = list()
-    st.append(i)
-    while len(st) > 0:
-        k = st.pop()
-        visited[k] = True
-        result_num.append(k)
-        for j in graph[k]:
-            if not visited[j]:
-                st.append(j)
-# print(f'{result_num=}')
-num_to_cow = {v:k for k, v in cows_dic.items()}
+# The official solution is fine, but the binary search is more intuitive
+# Within position [1,i], there is a responding numerical number j excluding fizz, buzz, fizzbuzz
+# The goal is to find the smallest position i, whose number is N
+low_pos = 1
+high_pos = 1_000_000_000_00 # Overestimate upper bound
+while low_pos < high_pos:
+    mid_pos = (low_pos+high_pos)//2
+    j = get_numbers(mid_pos)
+    if j < N:
+        low_pos = mid_pos + 1
+    else:
+        high_pos = mid_pos
+    # print(low_pos, high_pos)
 
-with open('lineup.out', 'w') as f:
-    for i in result_num:
-        print(num_to_cow[i])
-        f.write(num_to_cow[i]+'\n')
+result = low_pos
+print(result)
+with open('moobuzz.out', 'w') as f:
+    f.write(f'{result}\n')
