6ff5ba757b9ae6e695099ff9a6f15b4889305323
bluewater <70036495+Victor-wind@users.noreply.github.com>
2025-12-09 16:18:27 -0600

    Update distance.py

--- solution/2020/open/distance.py
+++ solution/2020/open/distance.py
@@ -1,3 +1,5 @@
+# All tests pass within 1200 ms
+
 '''
 with open('socdist.in') as f:
     lines = f.readlines()
@@ -18,6 +20,13 @@ with open('socdist.in', 'r') as r:
 intervals.sort()
 	
 # check whether can places N cows with distance d
+# The greedy approach is equivalent to the solution as stated “A solution with D greater than 0 is guaranteed to exist.”
+# Suppose the solution places the cows at positions S1, S2, ... Sn on the line. 
+# Step 1: Move cow 1 from S1 to the leftmost valid position G1 in the allowed interval. So G1<=S1, -> (S2-G1)>=(S2-S1)>=D;
+# Step 2: Move cow 2 from S2 to the left as much as possible G2. So G2<=S2, and (G2-G1)>=D, -> (S3-G2) >= (S3-S2)>=D
+# Continue this process for all cows.
+# In the end, the sequence S1, S2, ... Sn becomes G1, G2, ... Gn, and the new positions still satisfy all the required distance constraints.
+# Thus, the greedy method produces a valid solution equivalent to the original one.
 def check_dist(N: int, d: int) -> bool:
     cnt = 1
     pos = intervals[0][0]
