cca1fa1422141209fbdc761b5029ec9c31ab5200
bluewater <70036495+Victor-wind@users.noreply.github.com>
2026-05-23 23:26:25 -0500

    Update cereal.py

--- solution/2022/jan/cereal.py
+++ solution/2022/jan/cereal.py
@@ -1,9 +1,93 @@
-'''
-It is a difficult question: not too hard to understand the offical solution, but not easy to find the correct order.
-The idea is to find a loop (if any), break the loop/edge,
-assign the vertice, then loop again from the assigned vertice. Along the looping, assign all vertices  
-'''
+# Test case 7 times out, other tests cases pass within 1500 ms
 
+import sys
+from collections import deque
+
+N, M = [int(x) for x in sys.stdin.readline().split()]
+graph = [ list() for _ in range(M+1)]
+cows = [(-1,-1)]
+# the graph could have parallel edges between vertex, A<->B
+# to detect the cycle, each edge has an ID, which is the cow id
+for i in range(N):
+    f,s = [int(x) for x in sys.stdin.readline().split()]
+    cows.append((f,s))
+    graph[f].append((s, i+1)) # (s, and edge_id/cow_id)
+    graph[s].append((f, i+1)) # (f, and edge_id/cow_id)
+
+def bfs_find_cycle(i):
+    local_visited = [0] * (M+1)
+    if i in local_visited: return None
+    que = deque()
+    que.append((i, -1))
+    local_visited[i] = 1
+    while que:
+        v, p_edge = que.popleft()
+        for n, e_id in graph[v]: # e_id is the cow id
+            if e_id == p_edge:
+                continue
+            if local_visited[n]:
+                return e_id
+            local_visited[n] = 1
+            que.append((n,e_id))
+    return None
+
+cow_orders = list()
+def bfs_find_permutation(i):
+    que = deque()
+    # the parent edge is not necessary, as doesn't need to detect cycle
+    que.append(i)
+    visited[i] = 1
+    while que:
+        v = que.popleft()
+        for n, e_id in graph[v]: # e_id is the cow id
+            if e_id in removed_edge: # edge is removed to break the cycle
+                continue
+            if visited[n]:
+                continue
+            visited[n] = 1
+            que.append(n)
+            # cereal (n) is assigned to e_id/cow, as it is the only available cereal,
+            # the other cereal (v) is already assigned!
+            cow_orders.append(e_id)
+
+def remove_edges(cow_id):
+    # cow_id is the same as edge id
+    f,s = cows[cow_id]    
+    removed_edge.add(cow_id)
+    # need to keep other parallel edges
+    
+removed_edge = set() # to break cycles in bfs_find_permutation, need to remove some edges
+visited = [0] * (M+1)
+for i in range(M+1):
+    if visited[i] or not graph[i]:
+        continue # if vertex has been visited or has No edge
+    cow_id = bfs_find_cycle(i)
+    if cow_id:
+        f,s = cows[cow_id]
+        cow_orders.append(cow_id)
+        remove_edges(cow_id) # cow_id is the same as edge id, and assign cereal f to cow_id
+        # break the cycle
+        # the idea is to make the searching/start vertex unavailbe, so the cow/edge must use the other vertex 
+        bfs_find_permutation(f)
+        # print(f'Cycle with {f=}')
+        # print(cow_orders)
+    else:
+        bfs_find_permutation(i)
+        # print(f'No cycle with {i=}')
+        # print(cow_orders)
+
+print(N-len(cow_orders))
+
+order_str = '\n'.join((str(x) for x in cow_orders))
+
+cow_orders_set = set(cow_orders)
+hungry_cows = [i for i in range(1, N+1) if i not in cow_orders_set]
+hungry_cows_sr = '\n'.join((str(x) for x in hungry_cows))
+
+print(order_str)
+print(hungry_cows_sr)
+
+'''
 from collections import deque
 import os
 import sys
@@ -136,3 +220,4 @@ for m in cow_orders:
 for i in range(1,(N+1)):
     if cow_added[i] == 0:
         print(i)
+'''
