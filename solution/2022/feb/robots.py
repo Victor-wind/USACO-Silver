ff52962f825b8db63aa76a1ce0fdbdecde319e6e
bluewater <70036495+Victor-wind@users.noreply.github.com>
2026-06-07 10:02:31 -0500

    Update robots.py

--- solution/2022/feb/robots.py
+++ solution/2022/feb/robots.py
@@ -1,87 +1,55 @@
-import datetime
 
 '''
 *  
-1         2        3       4        5   6     7         8      9          10      11       12        13        14      15        16
-12.7mb   12.7mb  12.7mb   12.7mb    t   t   116.3mb  585.6mb  623.7mb   624.2mb   624.4mb  624.5mb   624.4mb  630.5mb  339.3mb   t
-79ms     83ms     81ms    81ms              3532ms   4832ms    4665ms   4721ms    4726ms   4677ms    4687ms   4805ms   3029ms
+1         2        3       4        5         6        7         8       9          10      11       12        13        14       15        16
+12.7mb   12.7mb  12.7mb   12.7mb    137.6mb   194.9mb  210.5mb  556.7mb  605.4mb   605.8mb   624.4mb  624.5mb   624.4mb  630.5mb  421.4mb   216.7mb
+69ms     72ms     71ms    81ms      5548ms    6050ms   6407ms   5104ms   4668ms    4803ms    4726ms   4677ms    4687ms   4805ms   3715ms    5949ms
 *
 '''
 
+from collections import defaultdict
+import sys
+
+input = sys.stdin.readline
+
 N = int(input())
-dst_x, dst_y = [int(x) for x in input().split()]
+xg, yg = map(int, input().split())
 
-instructions_1 = list()
-instructions_2 = list()
+a = [tuple(map(int, input().split())) for _ in range(N)]
 
-for i in range(N//2):
-    instruction = [int(x) for x in input().split()]
-    instructions_1.append(instruction)
-    
-for i in range(N//2, N):
-    instruction = [-int(x) for x in input().split()]
-    instructions_2.append(instruction)    
+m = N // 2
+L = a[:m]
+R = a[m:]
 
-time1 = datetime.datetime.now()
+right_map = defaultdict(lambda: [0] * (len(R) + 1))
 
-'''
-print(f'{N=}')
-print(f'{instructions_1=}')
-print(f'{instructions_2=}')
-'''
+cur = [(0, 0, 0)]  # sx, sy, size
 
-def move_to_list(ins, new_pos_dic, old_pos, counts):
-    delta_x, delta_y = ins
-    x, y = old_pos
-    x += delta_x
-    y += delta_y
-    key = (x, y)
-    value = [ i+1 for i in counts]
-    new_pos_dic[key] = value 
-    return
+for x, y in R:
+    nxt = []
+    for sx, sy, sz in cur:
+        nxt.append((sx, sy, sz))
+        nxt.append((sx + x, sy + y, sz + 1))
+    cur = nxt
 
-def merge_dic_list(pos, pos1):
-    for k, v in pos1.items():
-        if k in pos:
-            pos[k].extend(v)
-        else:
-            pos[k] = v
-    return
+for sx, sy, sz in cur:
+    right_map[(sx, sy)][sz] += 1
 
-# get all positions the robot reaches using instructions.
-# be aware that each instruction can be used at most once.
-# result data structure { (x,y): [cnt1, cnt2 ...]} -> to reach position (x,y),
-#       there are 2 ways: way 1 needs cnt1 instructions; way 2 needs cnt2 instructions; ... 
-# the issue for this data structure is test cases 5, 6, 16 times out.
-# the reason is that for the above test cases, there are too many duplicated cnts.
-# To reach (Xk, Yk), the results may be [1,1,1,.....1, 2,....2, ...].
-# Python is not efficient looping through the list to add them together in the last step. 
-# 
-# using collections.Counter to store result structure has other problem. test cases [8,9,10,11,12,13,14] times out.
-# the algorithm creates many Counter, which is less efficient than list.  
+ans = [0] * (N + 1)
 
-def move_list(instructions, start):
-    x0,y0 = start
-    pos = { (x0,y0): [0]}
-    for ins in instructions:
-        tmp_pos = dict()
-        for k,v in pos.items():
-            move_to_list(ins, tmp_pos, k, v)
-        merge_dic_list(pos, tmp_pos)
-    return pos
+cur = [(0, 0, 0)]
 
-pos_1 = move_list(instructions_1, (0,0))
-pos_2 = move_list(instructions_2, (dst_x, dst_y))
-#print(f'{pos_1=}')
-#print(f'{pos_2=}')
-nums_ways = [0]*(1+N)
-for k, v in pos_1.items():
-    if k in pos_2:
-        #print(f'find {k} {v} {pos_2[k]}')
-        for cnt1 in v:
-            for cnt2 in pos_2[k]:
-                nums_ways[cnt1+cnt2]+=1
-                
-for i in range(1, N+1):
-    print(nums_ways[i])
- 
+for x, y in L:
+    nxt = []
+    for sx, sy, sz in cur:
+        nxt.append((sx, sy, sz))
+        nxt.append((sx + x, sy + y, sz + 1))
+    cur = nxt
+
+for sx, sy, sz in cur:
+    vec = right_map.get((xg - sx, yg - sy))
+    if vec:
+        for rsz, cnt in enumerate(vec):
+            ans[sz + rsz] += cnt
+
+sys.stdout.write("\n".join(map(str, ans[1:])))
